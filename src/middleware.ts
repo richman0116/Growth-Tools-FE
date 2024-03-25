fb1b91c226cdbdede7221d3349265ed16e9b776b
richman0116 <maria8partee@gmail.com>
2024-03-26 06:58:49 +0700

    Add @tanstack/react-table package and remove unused code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
-import { JwtPayload, jwtDecode } from "jwt-decode";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { TOKEN } from "./helpers/cookie";
-import "core-js/stable/atob";
-import { dashboardNavigation } from "./navigation";
 
 export async function middleware(request: NextRequest, response: NextResponse) {
   if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/dashboard/trending-tools", request.url));
   }
-  
+
   if (request.nextUrl.pathname === "/dashboard") {
     return NextResponse.redirect(new URL("/dashboard/trending-tools", request.url));
   }
