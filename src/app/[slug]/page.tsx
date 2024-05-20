d2564ce26771d3b92ac6f73299ebd85faf679b4b
richman0116 <maria8partee@gmail.com>
2024-05-19 23:03:51 -0500

    reduce loading speed

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,180 +1,36 @@
-"use client";
+import Dashboard from "@/components/Dashboard"
 
-import { FilterPopoverTool } from "@/components/admin/filter-popover-tool";
-import { ToolCardInfo } from "@/components/cards/tool-card-info";
-import { Button } from "@/components/ui/button";
-import { Grid3X3, TableProperties } from "lucide-react";
-import { useEffect, useState } from "react";
-import {
-  filterTool,
-  getCategoryByHandle,
-  getCategoryById,
-  getCategoryList,
-} from "@/services/tool";
-import { usePathname } from "next/navigation";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useGlobalStoreContext } from "../../hooks/GlobalStoreContext";
-import MarketingToolHero from "@/components/marketingTools/MarketingToolHero";
-import BreadCrumb from "@/components/marketingTools/BreadCrumb";
-
-export default function Page() {
-  const pathName = usePathname();
-  const categoryHandle = pathName.split("/")[1];
-
-  const { setToolsListLoading } = useGlobalStoreContext();
-  const [categoryId, setCategoryId] = useState<string>("");
-  const [category, setCategory] = useState<Category | undefined>(undefined);
-  const [tools, setTools] = useState<ToolInfo[]>([]);
-  const [page, setPage] = useState(1);
-  const [take] = useState(10);
-  const [sort, setSort] = useState<string | undefined>(undefined);
-  const [order, setOrder] = useState<"ASC" | "DESC">("ASC");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [pagination, setPagination] = useState<PaginationMeta>({
-    page: 0,
-    take: 0,
-    itemCount: 0,
-    pageCount: 0,
-    hasPreviousPage: false,
-    hasNextPage: false,
-  });
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  const [variant, setVariant] = useState<"default" | "thumbnail">("default");
-
-  const onNextPage = () => {
-    setPage(page + 1);
-  };
-
-  const onFilter = (
-    category?: Category,
-    sort?: string,
-    order?: "ASC" | "DESC"
-  ) => {
-    if (category) {
-      setCategoryId(category.id);
-      getCategoryById(category.id).then((res) => {
-        setCategory(res);
-      });
-    }
-    if (sort) {
-      setSort(sort);
-    }
-    if (order) {
-      setOrder(order);
-    }
-  };
-
-  useEffect(() => {
-    setToolsListLoading(true);
-    const initFetchingData = async () => {
-      const [cateListInfos] = await Promise.all([
-        getCategoryList(),
-      ]);
-      const cateInfo = cateListInfos.filter((cateListInfo) => cateListInfo.handle === "/"+categoryHandle)
-      setCategoryId(cateInfo[0]?.id);
-      setCategory(cateInfo[0]);
-      setCategories(cateListInfos);
-    };
-    initFetchingData().then(() => {
-      setToolsListLoading(false);
-    });
-  }, [categoryHandle, setToolsListLoading]);
-
-  useEffect(() => {
-    setToolsListLoading(true);
-    setIsLoading(true);
-    if (!categoryId) return;
-
-    filterTool({
-      order,
-      page,
-      take,
-      sort,
-      categoryId,
-    }).then((res) => {
-      setTools(res?.data);
-      setPagination(res?.pagination);
-      setToolsListLoading(false);
-      setIsLoading(false);
-    });
-  }, [order, page, sort, take, categoryId, setToolsListLoading]);
-
-  return (
-    <>
-      {
-        category?.name === "Trending Tools" ?
-          <MarketingToolHero />
-          :
-          <BreadCrumb pagination={pagination} category={category} />
-      }
-
-      <section className="h-auto min-h-[70vh] flex flex-col gap-6 p-4 md:p-8">
-        <div className="flex gap-4 items-center">
-          <FilterPopoverTool
-            categories={categories}
-            onSubmitFilter={onFilter}
-          />
-
-          <span className="font-medium font-sm text-gray-500">Show as:</span>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="p-0 w-auto h-auto"
-            onClick={() => setVariant("default")}
-          >
-            <TableProperties
-              color={variant === "default" ? "#164CD9" : "black"}
-            />
-          </Button>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="p-0 w-auto h-auto"
-            onClick={() => setVariant("thumbnail")}
-          >
-            <Grid3X3 color={variant === "thumbnail" ? "#164CD9" : "black"} />
-          </Button>
-        </div>
-        {!tools?.length && !isLoading && (
-          <div
-            className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
-            role="alert"
-          >
-            <p className="font-bold">Ops</p>
-            <p className="text-sm">We have no tool to show.</p>
-          </div>
-        )}
-        <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-          {isLoading && (
-            <div className="flex items-center space-x-4">
-              <Skeleton className="h-12 w-12 rounded-full" />
-              <div className="space-y-2">
-                <Skeleton className="h-4 w-[250px]" />
-                <Skeleton className="h-4 w-[200px]" />
-              </div>
-            </div>
-          )}
-          {!!tools?.length &&
-            !isLoading &&
-            tools.map((tool) => (
-              <ToolCardInfo
-                key={`tool-card-${tool.id}`}
-                variant={variant}
-                tool={tool}
-                isLoading={false}
-              />
-            ))}
-        </div>
-        <div className="flex justify-center">
-          {pagination?.hasNextPage && (
-            <Button variant="outline" onClick={onNextPage}>
-              View More
-            </Button>
-          )}
-        </div>
-      </section>
-    </>
-  );
+interface GenericPageProps {
+  params: { slug: string }
 }
+
+const fetchCategoryLists = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/list`);
+  
+  if (!res.ok) {
+    throw new Error('Data fetching was failed')
+  }
+
+  return res.json()
+}
+
+const fetchFilterTools = async (id: any) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tools/filter?page=1&take=10&order=ASC&categoryId=${id}`)
+
+  if (!res.ok) {
+    throw new Error('Data fetching was failed on filter')
+  }
+
+  return res.json()
+}
+
+const GenericPage = async ({ params: { slug } }: GenericPageProps) => {
+  const categories = await fetchCategoryLists();
+
+  const cateInfo = categories.result.filter((category:Category) => category.handle === '/' + slug)?.[0]
+  const filterTools = await fetchFilterTools(cateInfo.id);
+
+  return <Dashboard categoryLists={categories.result} filterTools={filterTools.result} />
+}
+
+export default GenericPage
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,25 +1,40 @@
-"use client";
-import ANNOUNCEMENT from "@/assets/images/announcement.png";
+'use client'
 import { FilterPopoverTool } from "@/components/admin/filter-popover-tool";
 import { ToolCardInfo } from "@/components/cards/tool-card-info";
 import { Button } from "@/components/ui/button";
 import { Grid3X3, TableProperties } from "lucide-react";
-import Image from "next/image";
+import { useEffect, useState } from "react";
+import {
+  filterTool,
+  getCategoryById,
+  getCategoryList,
+} from "@/services/tool";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGlobalStoreContext } from "../../hooks/GlobalStoreContext";
+import MarketingToolHero from "@/components/marketingTools/MarketingToolHero";
+import BreadCrumb from "@/components/marketingTools/BreadCrumb";
 
-export default function MarketingPage() {
+interface IDashboard {
+  categoryLists: Category[]
+  filterTools: any
+}
+
+export default function Dashboard({categoryLists, filterTools}: IDashboard) {
+  
   const pathName = usePathname();
-  const keyPage = pathName.split("/")[1];
-  const categoryHandle = pathName.split("/")[2];
+  const categoryHandle = pathName.split("/")[1];
 
+  const { setToolsListLoading } = useGlobalStoreContext();
   const [categoryId, setCategoryId] = useState<string>("");
+  const [category, setCategory] = useState<Category | undefined>(undefined);
   const [tools, setTools] = useState<ToolInfo[]>([]);
   const [page, setPage] = useState(1);
   const [take] = useState(10);
   const [sort, setSort] = useState<string | undefined>(undefined);
   const [order, setOrder] = useState<"ASC" | "DESC">("ASC");
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [pagination, setPagination] = useState<PaginationMeta>({
     page: 0,
     take: 0,
@@ -43,6 +58,9 @@ export default function MarketingPage() {
   ) => {
     if (category) {
       setCategoryId(category.id);
+      getCategoryById(category.id).then((res) => {
+        setCategory(res);
+      });
     }
     if (sort) {
       setSort(sort);
@@ -52,38 +70,39 @@ export default function MarketingPage() {
     }
   };
 
+  useEffect(() => {
+    const initFetchingData = async () => {
+      const cateListInfos = categoryLists;
+      const cateInfo = cateListInfos.filter((cateListInfo) => cateListInfo.handle === "/"+categoryHandle)
+      setCategoryId(cateInfo[0]?.id);
+      setCategory(cateInfo[0]);
+      setCategories(cateListInfos);
+    };
+    initFetchingData().then(() => {
+    });
+  }, [categoryHandle, categoryLists, setToolsListLoading]);
+
+  useEffect(() => {
+    setToolsListLoading(true);
+    setIsLoading(true);
+    if (!categoryId) return;
+
+    setTools(filterTools?.data);
+    setPagination(filterTools?.pagination);
+    setToolsListLoading(false);
+    setIsLoading(false);
+  }, [setToolsListLoading, filterTools.data, filterTools?.pagination, categoryId, filterTools]);
+
   return (
     <>
-      <section className="relative">
-        <Image
-          src={ANNOUNCEMENT}
-          width={427}
-          className="absolute right-0 hidden md:hidden lg:block"
-          alt="Announcement"
-          style={{ width: '100%', height: '100%' }}
-          priority
-        />
-        <div className="py-[52px] md:px-12 md:max-w-4xl max-w-full px-4">
-          <h1 className="font-extrabold text-4xl lg:text-5xl mb-6">
-            Discover <span className="textGradient">marketing tools</span> that{" "}
-            <br />
-            supercharge your growth
-          </h1>
-          <p className="mb-6">
-            Browse through hundreds of unique tools to boost your marketing &
-            startup. Start by clicking Categories below to pick tools in
-            different marketing topics.
-          </p>
-          <div className="flex gap-2 md:gap-6">
-            <Button className="font-bold h-12">View Latest Tools</Button>
-            <Button variant="outline" className="h-12">
-              Trending tools
-            </Button>
-          </div>
-        </div>
-      </section>
+      {
+        category?.name === "Trending Tools" ?
+          <MarketingToolHero />
+          :
+          <BreadCrumb pagination={pagination} category={category} />
+      }
 
-      <section className="h-svh flex flex-col gap-6 p-4 md:p-8">
+      <section className="h-auto min-h-[70vh] flex flex-col gap-6 p-4 md:p-8">
         <div className="flex gap-4 items-center">
           <FilterPopoverTool
             categories={categories}
@@ -110,25 +129,23 @@ export default function MarketingPage() {
             <Grid3X3 color={variant === "thumbnail" ? "#164CD9" : "black"} />
           </Button>
         </div>
-        <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {!tools?.length && !isLoading && (
-            <div
-              className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
-              role="alert"
-            >
-              <p className="font-bold">Ops</p>
-              <p className="text-sm">We have no tool to show.</p>
-            </div>
-          )}
+        {!tools?.length && !isLoading && (
+          <div
+            className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
+            role="alert"
+          >
+            <p className="font-bold">Ops</p>
+            <p className="text-sm">We have no tool to show.</p>
+          </div>
+        )}
+        <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8">
           {isLoading && (
-            <div role="status" className="max-w-sm animate-pulse">
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-              <span className="sr-only">Loading...</span>
+            <div className="flex items-center space-x-4">
+              <Skeleton className="h-12 w-12 rounded-full" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
             </div>
           )}
           {!!tools?.length &&
