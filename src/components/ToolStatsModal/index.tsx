9e34caa14b2ca2ae419898a08a867c171d914309
richman0116 <maria8partee@gmail.com>
2024-05-29 13:38:36 -0500

    update super admin dashboard

--- src/components/ToolStatsModal/index.tsx
+++ src/components/ToolStatsModal/index.tsx
@@ -2,31 +2,32 @@ import { useCallback, useEffect, useState } from "react"
 import { Save } from "lucide-react";
 
 export interface ToolStats {
-  tool_view_count: number
-  monthly_view_count: any
-  clap_count: number
-  deal_count: number
+  clap_count: any
+  order: any
+  average_rating: any
 }
 
 const ToolStatsModal = ({ initialToolStats, onHide, onSave }: { initialToolStats: ToolStats, onHide: any, onSave: any}) => {
   const [toolStats, setToolStats] = useState(initialToolStats)
-  const isClapCountChanged = initialToolStats.clap_count !== toolStats.clap_count
+  const isToolInfoChanged = ((initialToolStats.clap_count !== toolStats.clap_count) || (initialToolStats.order !== toolStats.order) || (initialToolStats.average_rating !== toolStats.average_rating))
 
   useEffect(() => {
     setToolStats(initialToolStats)
   }, [initialToolStats])
 
   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setToolStats((toolStats) => ({
-      ...toolStats,
-      [e.target.name]: Number(e.target.value)
-    }))
+    if (isNaN(Number(e.target.value)) || e.target.value === null) {
+      setToolStats((toolStats) => ({
+        ...toolStats,
+        [e.target.name]: 0
+      }))
+    } else {
+      setToolStats((toolStats) => ({
+        ...toolStats,
+        [e.target.name]: Number(e.target.value)
+      }))
+    }
   }, [setToolStats])
-
-  // const handleSave = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault(),
-  //   onSave(toolStats)
-  // }, [toolStats, onSave])
   
   return (
     <div
@@ -74,31 +75,15 @@ const ToolStatsModal = ({ initialToolStats, onHide, onSave }: { initialToolStats
                   htmlFor="price"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Total Views
+                  Order
                 </label>
                 <input
                   type="number"
-                  name="total_views"
-                  value={toolStats.tool_view_count}
+                  name="order"
+                  value={toolStats.order}
+                  onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   required
-                  disabled
-                />
-              </div>
-              <div className="col-span-2 sm:col-span-1">
-                <label
-                  htmlFor="price"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Monthly Total Views
-                </label>
-                <input
-                  type="number"
-                  name="monthly_total_views"
-                  value={toolStats.monthly_view_count[0]}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  required
-                  disabled
                 />
               </div>
               <div className="col-span-2 sm:col-span-1">
@@ -122,22 +107,25 @@ const ToolStatsModal = ({ initialToolStats, onHide, onSave }: { initialToolStats
                   htmlFor="price"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Total Deals
+                  Average Rating
                 </label>
                 <input
                   type="number"
-                  name="total_deals"
-                  value={toolStats.deal_count}
+                  name="average_rating"
+                  value={toolStats.average_rating}
+                  onChange={handleChange}
+                  min={0}
+                  max={5}
+                  step={0.1}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   required
-                  disabled
                 />
               </div>
             </div>
             <button
               className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={(e) => {e.preventDefault(), onSave(toolStats)}}
-              disabled={!isClapCountChanged}
+              disabled={!isToolInfoChanged}
             >
               <Save className='w-4 mr-2' />
               Save
